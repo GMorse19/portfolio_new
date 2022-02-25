d51fe628d2b81da8f8d09fab381daeb0ed76aa94
GMorse19 <malorse@comcast.net>
2022-02-24 22:56:40 -0500

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          GPORT.
+        </p>
+        <a
+          className="App-link"
+          href="https://gmorse19.github.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          INIT COMMIT
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
