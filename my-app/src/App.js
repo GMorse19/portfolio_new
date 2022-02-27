969022502e4f1c65991894942c5da393abe67147
GMorse19 <malorse@comcast.net>
2022-02-27 09:38:54 -0500

    Header build

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
 
+import Header from './Components/Header/Header'
 import HomePage from './Pages/HomePage/HomePage'
 
 function App() {
   return (
     <div className="App">
+        <Header />
       <header className="App-header">
         <HomePage />
       </header>
