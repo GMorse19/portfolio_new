4d68018469f09731dc51138c82f0bc02e5422b84
GMorse19 <malorse@comcast.net>
2022-02-28 20:40:44 -0500

    Floating buttons
    Added hover stop

--- my-app/src/helpers/RandomMove/RandomMove.js
+++ my-app/src/helpers/RandomMove/RandomMove.js
@@ -7,12 +7,19 @@ import './RandomMove.scss'
 
 
 const RandomMove = () => {
-
+const [diva, setDiva] = useState(true)
+const [divb, setDivb] = useState(true)
+const [divc, setDivc] = useState(true)
+const [divd, setDivd] = useState(true)
+// Thank you to Amit Rogye --> https://codepen.io/cooljockey/pen/yLjXYz
+useEffect(() => {
 $(document).ready(function(){
     animateDiv('.a');
     animateDiv('.b');
     animateDiv('.c');
+    animateDiv('.d');
 });
+})
 
 const makeNewPosition = () => {
     // Get viewport dimensions (remove the dimension of the div)
@@ -28,22 +35,44 @@ const makeNewPosition = () => {
 
 const animateDiv = (myclass) => {
     const newq = makeNewPosition()
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 2000,   function(){
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 8000,   function(){
       animateDiv(myclass)
     })
 }
 
- const handleClick = () => animateDiv('.d')
- const handleStop = () => $('.d').stop()
+ const handleClick = (id) => {
+   console.log(id + 'this is the ID')
+   console.log(divd)
+   if (divd) {
+       $(id).stop()
+       setDivd(false)
+   }
+   else {
+     console.log('start')
+     animateDiv(id)
+     setDivd(true)
+   }
+ }
+ const handleStop = (id) => $(id).stop()
+ const handleStart = (id) => animateDiv(id)
 
   return (
     <div>
-    <div className='d'><Button onClick={handleClick}>Start</Button></div>
-    <div className='a'><Button onClick={handleStop}>Stop</Button></div>
-
-      <div className='b'></div>
-      <div className='c'></div>
-
+        <div className='a'>
+          <Button
+            className='circle-button'
+            href='https://github.com/GMorse19'
+            target="_blank" onMouseLeave={() => handleStart('.a')} onMouseEnter={() => handleStop('.a')}>Github</Button>
+        </div>
+      <div className='b'>
+        <Button className='circle-button' onMouseLeave={() => handleStart('.b')} onMouseEnter={() => handleStop('.b')}>Stop</Button>
+      </div>
+      <div className='c'>
+        <Button className='circle-button' onMouseLeave={() => handleStart('.c')} onMouseEnter={() => handleStop('.c')}>Stop</Button>
+      </div>
+      <div className='d'>
+        <Button className='circle-button' onMouseLeave={() => handleStart('.d')} onMouseEnter={() => handleStop('.d')}>Start</Button>
+      </div>
     </div>
   )
 }
