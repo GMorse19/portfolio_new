bfa353411d91729ab6bdddb17d0a163521f56fac
GMorse19 <malorse@comcast.net>
2022-05-30 16:31:28 -0400

    Added bubbles

---
+++ my-app/src/Components/Ball/Ball.js
@@ -0,0 +1,13 @@
+import react from 'react'
+
+import  './Ball.scss'
+
+const Ball = () => (
+  <div>
+    <section className='stage'>
+      <figure className="ball bubble"><span  className='eight'></span></figure>
+    </section>
+  </div>
+)
+
+export default Ball
--- my-app/src/Pages/HomePage/HomePage.js
+++ my-app/src/Pages/HomePage/HomePage.js
@@ -5,6 +5,7 @@ import './HomePage.scss'
 
 import RandomMove from '../../helpers/RandomMove0/RandomMove'
 import StaticLinks from '../../Components/StaticLinks/StaticLinks'
+import Ball from '../../Components/Ball/Ball'
 
 const HomePage = () => {
   return(
@@ -20,6 +21,7 @@ const HomePage = () => {
           <Col lg={6}>
             <p id='profile-title'>I am a software developer </p><p id='profile-title'>from Boston Massachusetts</p>
           </Col>
+          
         </Row>
         <Row>
           <Col md={{ span: 3, offset: 5}} id="letter-g"></Col>
