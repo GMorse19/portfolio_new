46df35ba78d2f5758a759ba010d7cdc1d5810d85
GMorse19 <malorse@comcast.net>
2022-06-16 20:19:17 -0400

    Hover

---
+++ my-app/src/Components/ExplodingBall/ExplodingBall.js
@@ -0,0 +1,17 @@
+import react from 'react'
+
+import  './ExplodingBall.scss'
+
+const ExplodingBall = (props) => {
+  // console.log(props.diameter)
+
+  return (
+    <div>
+      <section className='stage' style={{ width: 300, height: 300 }}>
+        <figure style={{ animation: `bubble-anim 2s ease-out infinite` }} className="explodingBall bubble"><span  className='eight'></span></figure>
+      </section>
+    </div>
+  )
+}
+
+export default ExplodingBall
